0af1e08a832d0e2d26fd60a3c5fcb8107537f986
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-01-05 16:25:04 +0800

    vssr v1.20

--- part1.sh
+++ part1.sh
@@ -18,10 +18,10 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/l
 git clone -b lede --depth=1 https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
 # luci-app-openclash
-git clone -b v0.40.7-beta --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
-mv package/OpenClash/luci-app-openclash package/luci-app-openclash
-rm -rf package/OpenClash
+# git clone -b v0.40.7-beta --depth=1 https://github.com/vernesong/OpenClash.git package/OpenClash
+# mv package/OpenClash/luci-app-openclash package/luci-app-openclash
+# rm -rf package/OpenClash
 
 # luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone -b v1.20 --depth=1 https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
