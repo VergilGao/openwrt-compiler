1980595213a76bf1153aa336954522b78e936088
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-07-20 09:13:22 +0800

    增加 luci-app-adblock-plus

--- part1.sh
+++ part1.sh
@@ -21,8 +21,8 @@ git clone -b lede --depth=1 https://github.com/pymumu/luci-app-smartdns.git pack
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
-# 京东签到
-git clone -b v1.0.5 --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+# luci-app-adblock-plus
+git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 
-# dockerman
-git clone -b v0.5.13 --depth=1 https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+# 京东签到
+git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
