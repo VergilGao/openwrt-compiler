92da0165185cd717441cac2ef5c9c899cb9e8ff1
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-08-06 09:13:04 +0800

    向xiaomi-ac2100和hiwif3添加luci-theme-argon主题

--- xiaomi-ac2100/part1.sh
+++ xiaomi-ac2100/part1.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# luci-theme-argon
+git clone -b v2.2.5 --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
