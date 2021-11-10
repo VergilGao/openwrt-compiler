a2fb2f2954e56740d7407986579d0fbd655663a8
羽先生 <8655163+VergilGao@users.noreply.github.com>
2021-11-10 14:47:39 +0800

    迁移额外软件包至单独仓库

--- amd64/part1.sh
+++ amd64/part1.sh
@@ -1,15 +1,1 @@
 #!/bin/bash
-
-# ddns-scripts-dnspod OpenWrt 官方 DDNS 插件的 dnspod 更新脚本
-git clone --depth=1 https://github.com/VergilGao/ddns-scripts-dnspod package/ddns-scripts-dnspod
-
-# luci-app-adblock-plus 基于DNS的广告过滤
-git clone -b 1.0-8 --depth=1 https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
-
-# luci-theme-argon
-git clone -b v2.2.5 --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
-# OpenClash
-git clone --depth=1 https://github.com/vernesong/OpenClash /tmp/clash
-mv /tmp/clash/luci-app-openclash package/luci-app-openclash
