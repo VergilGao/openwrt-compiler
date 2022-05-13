7ab02431f7ca4e1643868502b414ff612c7fa285
羽先生 <8655163+VergilGao@users.noreply.github.com>
2022-05-13 11:45:27 +0800

    replace default theme to argon

--- amd64/part2.sh
+++ amd64/part2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# replace default theme to argon
+rm -rf feeds/luci/themes/luci-theme-bootstrap/
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci*/Makefile
+
 # Modify DHCP client for iptv
 sed -i 's/\${vendorid:+-V "$vendorid"}/-V \"\"/g' package/network/config/netifd/files/lib/netifd/proto/dhcp.sh
